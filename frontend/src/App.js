fbfbd4f3d3b0c7dd5169bbd63443bfd1dcc3c1e1
mogwai <feelirabs@gmail.com>
2020-03-26 10:56:26 +0100

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Good Faith Project</h1>
+      <h2>is now live</h2>
+      <button className="Button">Give</button>
+      <button className="Button Button2">Receive</button>
+    </div>
+  );
+}
+
+export default App;
