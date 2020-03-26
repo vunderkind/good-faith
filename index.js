02d16578a05a6076750dd78f14904402e864b644
mogwai <feelirabs@gmail.com>
2020-03-26 04:49:47 +0100

    Added dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const server = require('./server.js');
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(`Good Faith is Listening on port ${PORT}...`);
-});
+  console.log(`\n*** Good Faith is Listening on port ${PORT} ***\n`);
+})
