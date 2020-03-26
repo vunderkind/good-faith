b576035de4158d752a22d9486b145403fb81268f
mogwai <feelirabs@gmail.com>
2020-03-26 19:19:15 +0100

    removed react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import kindness from './kindness.png';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        Hello
-      </Router>
       <img className="App-logo " src={kindness} alt="kindness"/>
       <h1>Angels among us.</h1>
       <h2>'Angels among us' is a simple tool for people with relatively secure financial statuses to help people whose means of livelihood have been affected by the COVID-19 outbreak.<br/><br/> When you click the 'Be an angel' button, we'll send you a random list of ten people you can contribute to. We suggest that you send money to each of the ten (eg. N2,000-N10,000 to each person)</h2>
