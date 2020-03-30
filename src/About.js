85d2ac2f564f1c368e2252ba6cb27d467763067e
mogwai <feelirabs@gmail.com>
2020-03-30 23:20:46 +0100

    Add about page

--- src/About.js
+++ src/About.js
@@ -3,9 +3,19 @@ import './App.css'
 
 const About = () => {
     return(
-        <div>
-            <h2>About</h2>
-            <h3>Na wa oh</h3>
+        <div className="App2">
+            <h1>What is this website?</h1>
+            <h2>'Angels among us' is a simple tool for people looking for ways to share some of their income with people who are in dire economic straits because of the COVID-19 pandemic.</h2>
+            <h2>It is a simple database of people who need help, randomly delivered to you ten at a time. You can then copy their account details and make a contribution to their well-being.</h2>
+            <h2>None of your payments pass through me in any form. You are helping these distressed people directly, without my having to play a middleman's role. I make no profit from this, and I do not collect commissions. I earn nothing from this.</h2>
+            <br/>
+            <h1>How do I help?</h1>
+            <h2>On the homepage, click the 'Be an angel' button to see a randomly-generated list of 10 people who need help from you. Please send the same amount of money to all of them. This is a great way to ensure that as many people as possible are uniformly reached by kind people like you.</h2>
+            <h1>I need help</h1>
+            <h2>Click the 'I need help' button on the homepage and let people know just what you need</h2>
+            <h2>This is a good faith project: it works because honest people want to help honest people in need.</h2>
+            <h2>If your financial needs have been met, send me an email on thevunderkind@gmail.com so I can take out your name. This is important so other people in need are likelier to be helped.</h2>
+
         </div>
     )
 }
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import Home from './Home'
 import About from './About'
+import './App.css'
 
 const App = () => {
     return (
@@ -10,16 +11,16 @@ const App = () => {
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to="/users">Users</Link>
-        </Router>
 
         <Switch>
-        <Route path="/">
+        <Route exact path="/">
             <Home />
           </Route>
           <Route path="/about">
             <About />
           </Route>
         </Switch>
+        </Router>
         </>
     )
 }
