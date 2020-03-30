1bbcee5e5f66b9ff15dac8ca3b647d1b35a8f311
mogwai <feelirabs@gmail.com>
2020-03-30 22:14:17 +0100

    Build done

---
+++ build/precache-manifest.90a73e8ff74766b4dcb9b3927077e95d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac78553ef2798cedd4f1413c6d86e3eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afc5297a1c745fad211",
+    "url": "/static/css/main.5f97d690.chunk.css"
+  },
+  {
+    "revision": "abd20b446400ecfa323b",
+    "url": "/static/js/2.85794a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85794a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afc5297a1c745fad211",
+    "url": "/static/js/main.79107665.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "d6a67a899d1c5a4d2af130bc10b80cf8",
+    "url": "/static/media/kindness.d6a67a89.png"
+  }
+]);
