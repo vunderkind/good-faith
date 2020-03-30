808316c6d1f4bf9f9ebd4ffb4d68d1f080880906
mogwai <feelirabs@gmail.com>
2020-03-30 22:50:59 +0100

    Added routes

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const About = () => {
+    return(
+        <div>
+            <h2>About</h2>
+            <h3>Na wa oh</h3>
+        </div>
+    )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 import Home from './Home'
+import About from './About'
 
 const App = () => {
     return (
         <>
-        <Home/>
+        <Router>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to="/users">Users</Link>
+        </Router>
+
+        <Switch>
+        <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
         </>
     )
 }
