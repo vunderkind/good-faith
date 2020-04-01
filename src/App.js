e4359ab813f491adbad795449656c822fa336c64
Etin <etinosaobaseki@gmail.com>
2020-04-01 01:21:39 +0100

    Add social share

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import NeedHelp from './NeedHelp'
+import SocialShare from './SocialShare';
 import './App.css'
 
 const App = () => {
-    return (
-        <div>
-        <Router>
-            <div className="Linq">
-            <Link className="Link" id="home" to='/'>Home</Link>
-            <Link className="Link" to='/about'>About</Link>
-            <Link className="Link" to='/needhelp'>Need help</Link>
-            </div>
+  return (
+    <div>
+      <Router>
+        <div className="Linq">
+          <Link className="Link" id="home" to='/'>Home</Link>
+          <Link className="Link" to='/about'>About</Link>
+          <Link className="Link" to='/needhelp'>Need help</Link>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <h3>Share this with someone who needs it.</h3>
+          <SocialShare
+            text={"Share kindness in these COVID-19 times. If you're financially stable during this COVID-19 crisis, consider making a small contribution to people who haven't been as lucky. Use this to get a randomly-generated list of ten people who need your help."}
+            url={window.location.href}
+            tag={"#AngelsAmongUs"}
+          />
+        </div>
+
 
         <Switch>
-        <Route exact path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/about">
@@ -26,9 +36,9 @@ const App = () => {
             <NeedHelp />
           </Route>
         </Switch>
-        </Router>
-        </div>
-    )
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/SocialShare.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function SocialShare(props) {
+    return (
+        <div className="share-btns">
+            <a
+                href={"https://www.facebook.com/sharer/sharer.php?u=" + props.url + "&quote=" + encodeURI(props.text) + " #" + props.tag + ""}
+                className="social facebook btn"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-facebook"></i> Facebook
+            </a>
+            &nbsp;
+            <a
+                href={"https://twitter.com/intent/tweet?text=" + encodeURI(props.text) + "&url=" + props.url + "&hashtags=" + props.tag + ""}
+                className="social twitter btn"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-twitter"></i> Twitter
+            </a>
+            &nbsp;
+            <a
+                href={"whatsapp://send?text=" + encodeURI(props.text) + " " + props.url + ""}
+                className="social whatsapp btn"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <i className="fab fa-whatsapp"></i> WhatsApp
+            </a>
+            &nbsp;
+        </div>
+    )
+}
+
+export default SocialShare
