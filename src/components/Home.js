6064dafaee5d79e87704fca1908ad00e4f122790
Etin <etinosaobaseki@gmail.com>
2020-04-04 09:32:12 +0100

    Layout changes to keep CTAs visible

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,24 +41,24 @@ function Home() {
             <TextCenter>
               <p>
                 'Angels among us' is a simple tool for people with relatively secure financial statuses to help people whose means of livelihood have been affected by the COVID-19 outbreak.
-                    </p>
-              <p>
-                When you click the 'Be an angel' button, you'll get a random list of <span>three</span> people you can contribute to.
-                    </p>
-              <p>
-                Consider sending money to each person on the list (eg. N2,000 to three people is N6,000). This is a great way to ensure we reach as many people as possible, as uniformly as possible.
-                    </p>
+              </p>
               <Button type="primary" onClick={fetchusers}>Be an angel</Button>
               <Button type="secondary" link="needhelp">I need help</Button>
+              <p>
+                When you click the 'Be an angel' button, you'll get a random list of <span>three</span> people you can contribute to.
+              </p>
+              <p>
+                Consider sending money to each person on the list (eg. N2,000 to three people is N6,000). This is a great way to ensure we reach as many people as possible, as uniformly as possible.
+              </p>
               <p><a href="/faq#verify">See how we verify identities of people in need</a></p>
             </TextCenter>
           </Fragment>
           :
           <Fragment>
-            <div className="header">
-              <h2>A list of people who have been economically affected by the COVID-19 virus and are currently without pay.</h2>
-              <h2 style={{ color: 'gray', fontStyle: 'italic' }}>Here are <span>three</span> randomly-generated individuals who you can help.<br /> If you'd like to donate to more people after your first three, refresh the page.</h2>
-            </div>
+            <TextCenter>
+              <p>A list of people who have been economically affected by the COVID-19 virus and are currently without pay.</p>
+              <p className="accent-text">Here are <span>three</span> randomly-generated individuals who you can help.<br /> If you'd like to donate to more people after your first three, refresh the page.</p>
+            </TextCenter>
             <div>
               {!users ? <Loader className="Loader"
                 type="BallTriangle"
